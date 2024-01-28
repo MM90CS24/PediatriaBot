27ea85beb57d64567d8530815a899cbba39ecd14
Chichimokers <87203631+Chichimokers@users.noreply.github.com>
2024-01-27 20:46:26 -0500

    Update bot_conf.py

--- src/cfgs/bot_conf.py
+++ src/cfgs/bot_conf.py
@@ -1,7 +1,7 @@
 NAME = "Ped"
 HASH = "13b1607f156d7a7f0c755dbf4ea5061a"
 ID =28341593
-BOT_TK = "6587799120:AAGshHv1iSaUCD1Cal9BtzsiCwzSG533jHY"
+BOT_TK = "6587799120:AAHy5m6vwFo1zX2odV1nBuzuuncgxCzNrk0"
 ZIPS = 10
 
 # para meter mas usuarios solo meta el ID del usuario 
